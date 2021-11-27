208a88e8765683ab89331613a822700e168913d2
brenda maria <brendamaria177@gmail.com>
2021-11-26 23:41:25 -0300

    main e notas atualizado

--- aluno/src/aluno/Notas.java
+++ aluno/src/aluno/Notas.java
@@ -1,67 +1,126 @@
 package aluno;
 
+import javax.swing.JOptionPane;
+
 public class Notas {
-	
+	/***** ATRIBUTOS *********/
+
+
 	private double n1;
-	
 	private double n2;
-	
 	private double n3;
-	
 	private double n4;
-	
 	private double n5;
-	
 	private double media;
-	
+
+	/****************/
+
+	/***** GET E SET ***********/
+
+
 	public double getMedia() {
 		return media;
 	}
-
-	public double calculaMedia() {
-		double total = getN1() + getN2() + getN3() + getN4();
-			this.media = total/4;
-			return this.media;
+	
+	public void setMedia(double media) {
+		this.media = media;
 	}
-	
-	public double calculaMediaRecuperacao(double n5) {
-		double mediaFinal = (calculaMedia() + n5)/2;
-		this.media = mediaFinal;
-		return this.media;
-	} 
-	
+
 	public double getN1() {
 		return n1;
 	}
-	public void setN1(double n1) {
-		this.n1 = n1;
+
+	public void setN1(String nota1) {
+		nota1 = nota1.replace(",", ".");
+		double nota1D = Double.parseDouble(nota1);
+		if ( nota1D < 0 && nota1D > 10  ) {
+			System.out.println("Não aceitamos numeros menores que 0 e maior do que 10");
+		}else {
+			this.n1 = nota1D;
+		}
 	}
-	
+
 	public double getN2() {
 		return n2;
 	}
+
 	public void setN2(double n2) {
-		this.n2 = n2;
+		if ( n2 < 0 && n2 > 10  ) {
+			System.out.println("Não aceitamos numeros menores que 0 e maior do que 10");
+		}else {
+			this.n2 = n2;
+		}
 	}
-	
+
 	public double getN3() {
 		return n3;
 	}
+
 	public void setN3(double n3) {
-		this.n3 = n3;
+		if ( n3 < 0 && n3 > 10  ) {
+			System.out.println("Não aceitamos numeros menores que 0 e maior do que 10");
+		}else {
+			this.n3 = n3;
+		}
 	}
-	
+
 	public double getN4() {
 		return n4;
 	}
+
 	public void setN4(double n4) {
-		this.n4 = n4;
+		if ( n4 < 0 && n4 > 10  ) {
+			System.out.println("Não aceitamos numeros menores que 0 e maior do que 10");
+		}else {
+			this.n4 = n4;
+		}
 	}
-	
+
 	public double getN5() {
 		return n5;
 	}
+
 	public void setN5(double n5) {
-		this.n5 = n5;
+		if ( n5 < 0 && n5 > 10  ) {
+			System.out.println("Não aceitamos numeros menores que 0 e maior do que 10");
+		}else {
+			this.n5 = n5;
+		}
 	}
+
+	/*****************/
+
+	/***** MÉTODOS *********/
+
+	public double calculaMedia() {
+		double total = (getN1() + getN2() + getN3() + getN4())/4;
+		setMedia(total);
+		if (media >= 7.00) {
+			JOptionPane.showMessageDialog(null,"A média do aluno(a) é "+getMedia()+
+					"\n"+"Aluno(a) Aprovado.","Notas",JOptionPane.INFORMATION_MESSAGE);
+		} else if (media <= 4.00) {
+			JOptionPane.showInputDialog(null, "Você está reprovado nets materia, tente novamente no proximo semestre",
+					JOptionPane.INFORMATION_MESSAGE);
+		} else {
+			JOptionPane.showMessageDialog(null,"A média do aluno(a) é "+getMedia()+
+					"\n"+"Você está de recuperação, faça alguma atividade complementar para adiconar a sua media final.","Notas",JOptionPane.INFORMATION_MESSAGE);
+			String notaR = JOptionPane.showInputDialog("Digite a nota complementar: ");
+			setN5(Double.parseDouble(notaR));
+			calculaMediaRecuperacao(getN5());
+			if (getMedia() >= 7.00) {
+				JOptionPane.showMessageDialog(null,"A média do aluno(a) é "+getMedia()+
+						"\n"+"Parabéns você passou na recuperação.","Notas",JOptionPane.INFORMATION_MESSAGE);
+			} else {
+				JOptionPane.showMessageDialog(null,"A média do aluno(a) é "+getMedia()+
+						"\n"+"Você não passou na recuperação, tente novamente no proximo semestre.","Notas",JOptionPane.INFORMATION_MESSAGE);
+			}
+		}
+		return this.media;
+	}
+
+	public void calculaMediaRecuperacao(double notaR) {
+		double mediaFinal = (getMedia() + notaR) / 2;
+		setMedia(mediaFinal);
+	}
+	
 }
--- aluno/src/aluno/main.java
+++ aluno/src/aluno/main.java
@@ -1,30 +1,68 @@
 package aluno;
 
+import java.text.DecimalFormat;
+import java.util.Locale;
+
 import javax.swing.JOptionPane;
 
 public class main {
 
 	public static void main(String[] args) {
-		
+
 		Aluno aluno = new Aluno();
-		
-		double nota = 4.6;
 
-		String nome =  JOptionPane.showInputDialog("Digite o nome do aluno(a): ");
+		String nome = JOptionPane.showInputDialog("Digite o nome do aluno(a): ");
 		aluno.setNome(nome);
-		
-		aluno.setN1(Double.parseDouble(JOptionPane.showInputDialog("Digite a primeira nota: ")));
-		aluno.setN2(Double.parseDouble(JOptionPane.showInputDialog("Digite a segunda nota: ")));
-		aluno.setN3(Double.parseDouble(JOptionPane.showInputDialog("Digite a terceira nota: ")));
-		aluno.setN4(Double.parseDouble(JOptionPane.showInputDialog("Digite a quarta nota: ")));		
-		
-		aluno.calculaMedia();
-		
-		System.out.println(aluno.getMedia());
-		
-//		aluno.setN1(Double.);
-		
 
+		String nota1 = (JOptionPane.showInputDialog("Digite a primeira nota: "));
+		nota1 = nota1.replace(",", ".");
+		double nota1D = Double.parseDouble(nota1);
+		if (nota1D < 0 || nota1D > 10) {
+			JOptionPane.showMessageDialog(null,
+					"Voce digitou: " + nota1D + ". Não aceitamos números menores que 0 e maiores que 10", "Notas",
+					JOptionPane.INFORMATION_MESSAGE);
+		} else {
+			aluno.setN1(nota1D);
+
+		}
+
+		String nota2 = (JOptionPane.showInputDialog("Digite a segunda nota: "));
+		nota2 = nota2.replace(",", ".");
+		double nota2D = Double.parseDouble(nota1);
+		if (nota2D < 0 || nota2D > 10) {
+			JOptionPane.showMessageDialog(null,
+					"Voce digitou: " + nota2D + ". Não aceitamos números menores que 0 e maiores que 10", "Notas",
+					JOptionPane.INFORMATION_MESSAGE);
+		} else {
+			aluno.setN1(nota2D);
+		}
+
+		String nota3 = (JOptionPane.showInputDialog("Digite a terceira nota: "));
+		nota3 = nota3.replace(",", ".");
+		double nota3D = Double.parseDouble(nota1);
+		if (nota3D < 0 || nota3D > 10) {
+			JOptionPane.showMessageDialog(null,
+					"Voce digitou: " + nota3D + ". Não aceitamos números menores que 0 e maiores que 10", "Notas",
+					JOptionPane.INFORMATION_MESSAGE);
+		} else {
+			aluno.setN3(nota3D);
+
+		}
+
+		String nota4 = (JOptionPane.showInputDialog("Digite a quarta nota: "));
+		nota4 = nota4.replace(",", ".");
+		double nota4D = Double.parseDouble(nota1);
+		if (nota4D < 0 || nota4D > 10) {
+			JOptionPane.showMessageDialog(null,
+					"Voce digitou: " + nota4D + ". Não aceitamos números menores que 0 e maiores que 10", "Notas",
+					JOptionPane.INFORMATION_MESSAGE);
+		} else {
+			aluno.setN4(nota4D);
+		}
+
+		aluno.calculaMedia();
+
+		System.out.println(aluno.getN1());
 	}
 
 }
